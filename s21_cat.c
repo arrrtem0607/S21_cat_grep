c6aa637bdbdcc1ec8b6fff5812ec2cec826642b2
artem <DlyaNas@MacBook-Air.local>
2023-12-18 23:28:01 +0300

    'r5'

--- s21_cat.c
+++ s21_cat.c
@@ -8,18 +8,21 @@ typedef struct {
 } arguments;
 
 void parser(int argc, char** argv, arguments* arg);
-void output(int argc, char** argv);
+void output(int argc, char** argv, arguments* arg);
+void output_b(int argc, char** argv);
+void output_e(int argc, char** argv);
 void output_n(int argc, char** argv);
+void output_s(int argc, char** argv);
+void output_t(int argc, char** argv);
+void output_v(int argc, char** argv);
 
 int main(int argc, char** argv) {
-    arguments arg = {0};
-    parser(argc, argv, &arg);
-    /*if (arg -> n == 1) {
-        output_n(argc, argv);
-    } else {
-        output(argc, argv);
-    }*/
-    printf("\nb:%d e:%d v:%d n:%d s:%d t:%d", arg.b, arg.e, arg.v, arg.n, arg.s, arg.t);
+    arguments argos = {0};
+    arguments* arg = &argos;
+    parser(argc, argv, arg);
+    output(argc, argv);
+
+    //printf("\nb:%d e:%d v:%d n:%d s:%d t:%d", arg -> b, arg -> e, arg -> v, arg -> n, arg -> s, arg -> t);
     return 0;
 }
 
@@ -32,7 +35,7 @@ void parser(int argc, char** argv, arguments* arg) {
             {NULL, 0,                        NULL, 0}
     };
     while (flag != -1) {
-        flag = getopt_long(argc, argv, "benstET", long_options, NULL);
+        flag = getopt_long(argc, argv, "+benstET", long_options, NULL);
         switch (flag) {
             case 'b':
                 arg -> b = 1;
@@ -63,32 +66,65 @@ void parser(int argc, char** argv, arguments* arg) {
     }
 }
 
-void output(int argc, char** argv){
+void output(int argc, char** argv, arguments* arg){
     for (int i = optind; i < argc; i++) {
         FILE *f = fopen(argv[i], "r");
-        char *toprint = NULL;
-        size_t memtoprint = 0;
-        ssize_t read;
-        while ((read = getline(&toprint, &memtoprint, f)) != -1) {
-            printf("%s", toprint);
+        if (f){
+            int current;
+            int number = 1;
+            int empty_number = 1;
+            while ((current = fgetc(f)) != EOF){
+                if (arg -> b) {
+                    output_b();
+                }
+                if (arg -> e){
+                    output_e();
+                }
+                if (arg -> n){
+                    output_n();
+                }
+                if (arg -> s){
+                    output_s();
+                }
+                if (arg -> t){
+                    output_t();
+                }
+                if (arg -> v){
+                    output_v();
+                }
+            }
+            char *toprint = NULL;
+            size_t memtoprint = 0;
+            ssize_t read;
+            while ((read = getline(&toprint, &memtoprint, f)) != -1) {
+                printf("%s", toprint);
+            }
+            free(toprint);
+            fclose(f);
+        } else {
+            printf("%s: %s: No such file or directory\n",argv[0], argv[i]);
         }
-        free(toprint);
-        fclose(f);
     }
 }
 
-void output_n(int argc, char** argv){
-    for (int i = optind; i < argc; i++) {
-        FILE *f = fopen(argv[i], "r");
-        char *toprint = NULL;
-        size_t memtoprint = 0;
-        ssize_t read;
-        int stroka = 1;
-        while ((read = getline(&toprint, &memtoprint, f)) != -1) {
-            printf("%d %s", stroka, toprint);
-            stroka++;
-        }
-        free(toprint);
-        fclose(f);
-    }
+void output_b(int argc, char** argv, FILE* file){
+
+}
+void output_e(int argc, char** argv, FILE* file){
+
+}
+void output_n(int argc, char** argv, FILE* file){
+
+}
+void output_s(int argc, char** argv, FILE* file){
+
+}
+void output_t(int argc, char** argv, FILE* file){
+
+}
+void output_T(int argc, char** argv, FILE* file){
+
+}
+void output_E(int argc, char** arg, FILE* file){
+
 }
