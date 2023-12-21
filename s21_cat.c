c274b0c3d0eaaa13d68867d89afe2d6c5a4e4bed
Nail Lora <naillora@oa-m3.msk.21-school.ru>
2023-12-21 19:30:12 +0300

    r6

--- s21_cat.c
+++ s21_cat.c
@@ -8,12 +8,15 @@ typedef struct {
 } arguments;
 
 void parser(int argc, char** argv, arguments* arg);
-void openfile(int argc, char ** argv);
+void openfile(int argc, char ** argv, arguments* arg);
+void catnoflag(FILE* f);
+void cat_e(FILE* f);
+void cat_t(FILE* f);
 
 int main(int argc, char** argv) {
-    arguments* arg = NULL;
-    parser(argc, argv, arg);
-    openfile(argc, argv);
+    arguments arg;
+    parser(argc, argv, &arg);
+    openfile(argc, argv, &arg);
     return 0;
 }
 
@@ -57,16 +60,66 @@ void parser(int argc, char** argv, arguments* arg) {
     }
 }
 
-void openfile(int argc, char ** argv){
+void catnoflag(FILE* f){
+    int ch;
+    while ((ch = fgetc(f)) != EOF) {
+        fputc(ch, stdout);
+    }
+}
+
+void cat_e(FILE* f){
+    int ch;
+    while((ch = fgetc(f)) != EOF){
+        if (ch == '\n'){
+            fputc('$', stdout);
+        }
+        fputc(ch, stdout);
+    }
+}
+
+void cat_t(FILE* f){
+    int ch;
+    while((ch = fgetc(f)) != EOF){
+        if (ch == '\t'){
+            fputc('^', stdout);
+            fputc('I', stdout);
+            continue;
+        }
+        fputc(ch, stdout);
+    }
+}
+
+void cat_n(FILE* f){
+    int ch;
+    int count = 1;
+    while((ch = fgetc(f)) != EOF){
+        if (ch == '\n'){
+            count++;
+            fputc('\t', stdout);
+            fputc(count, stdout);
+        }
+        fputc(ch, stdout);
+    }
+}
+
+
+void openfile(int argc, char ** argv, arguments* arg){
     for (int i = optind; i < argc; i++) {
         FILE *f = fopen(argv[i], "r");
         if (f == NULL) {
             printf("%s: %s: No such file or directory\n", argv[0], argv[i]);
             exit(1);
         }
-        int ch;
-        while ((ch = fgetc(f)) != EOF) {
-            fputc(ch, stdout);
+        if (arg -> e) {
+            cat_e(f);
+        }
+        if (arg -> t) {
+            cat_t(f);
+        }
+        if (arg -> n) {
+            cat_n(f);
+        } else {
+            catnoflag(f);
         }
     }
 }
